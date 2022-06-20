bd8124e6199b76a55791c27dd00dfc835718c273
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2022-06-20 17:25:29 -0600

    basic work

--- flags.R
+++ flags.R
@@ -3,19 +3,37 @@
 library(tidyverse)
 
 
+
 pride = c('#FF0909','#FF9B3C','#FFEE39','#009A3A','#002F97','#5C1079')
 
+pansexual = c('#FF0579','#FFD333','#009FFB')
+
+gender_fluid = c('#FE6093','#AD00CF','#000000','#0620B1')
+
+transg = c('#00BFFB','#FFBFFD')
+
 df = tibble(
-  x = c(65,58,48,39,25,19),
-  y = rnorm(6, mean = 5, sd= 2)
+  x = c(1:2),
+  y = c(2:1) #rnorm(6, mean = 5, sd= 2)
 )
 
-ggplot(
-  df,
+df %>% 
+  ggplot(
   aes(x= x,
       y= y,
-      fill= pride)
+      fill= transg #gender_fluid #pansexual #pride
+      )
 )+
   geom_col()+
-  scale_fill_manual(values = pride)+
+  scale_fill_manual(values = transg #gender_fluid #pansexual # pride
+                    )+
+  # theme_classic()
+  # theme_classic()
   ggdark::dark_mode()
+
+
+
+
+
+
+
